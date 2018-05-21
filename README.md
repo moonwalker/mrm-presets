b84fd03d2a946c885d9c59a5059a5db936d87dba
Vio <vio@beanon.com>
2018-05-21 17:52:34 +0200

    Update react-component skeleton

--- README.md
+++ README.md
@@ -16,6 +16,19 @@ mrm react-component --preset @moonwalker/mrm-preset-react --config:slug top-navi
 
 // Running react-component...
 // Create src/components/signup/top-navigation/index.js
+// Create src/components/signup/top-navigation/top-navigation.component.js
+// Create src/components/signup/top-navigation/top-navigation.stories.js
+// Create src/components/signup/top-navigation/top-navigation.styl
+```
+
+Run the generator in the current dirrectory:
+
+```shell
+cd src/components
+mrm react-component --preset @moonwalker/mrm-preset-react --config:slug top-navigation --config:cwd . --config:prefix signup
+
+// Running react-component...
+// Create src/components/signup/top-navigation/index.js
 // Create src/components/signup/top-navigation/component.js
 // Create src/components/signup/top-navigation/stories.js
 // Create src/components/signup/top-navigation/style.styl
--- react-component/index.js
+++ react-component/index.js
@@ -5,49 +5,83 @@ const { camelCase, upperFirst, template } = require('lodash');
 
 const TEMPLATE_DIR = path.join(__dirname, 'templates');
 
-const generateFile = (base, options) => {
-  const { dir, slug, prefix } = options;
+const generateFile = (filename, options) => {
+  const {
+    dir,
+    slug,
+    prefix,
+    container,
+  } = options;
 
-  const templatePath = path.join(TEMPLATE_DIR, `${base}.txt`);
+  const templatePath = path.join(TEMPLATE_DIR, `${filename}.txt`);
   const compiled = template(fs.readFileSync(templatePath));
 
-  lines(path.join(dir, base))
+  const outputFilename = options.outputFilename || filename;
+
+  lines(path.join(dir, outputFilename))
     .add(compiled({
-      slug,
-      prefix,
       componentName: upperFirst(camelCase(slug)),
+      componentSlug: slug,
       storyPrefix: prefix.split('/').map(i => upperFirst(camelCase(i))).join('/'),
+      container,
     }))
     .save();
 };
 
-const task = (config) => {
-  const { src, slug, prefix } = config.defaults({ src: 'src' }).require('slug', 'prefix').values();
+const task = (mrmConfig) => {
+  const defaultConfig = {
+    cwd: process.cwd(),
+    container: false,
+  };
 
-  const componentDir = path.join(src, prefix, slug);
+  const {
+    cwd,
+    slug,
+    prefix,
+    container,
+  } = mrmConfig.defaults(defaultConfig).require('slug', 'prefix').values();
+
+  const componentDir = path.join(cwd, prefix, slug);
 
   generateFile('index.js', {
     dir: componentDir,
     slug,
     prefix,
+    container,
   });
 
   generateFile('component.js', {
     dir: componentDir,
+    outputFilename: `${slug}.component.js`,
     slug,
     prefix,
+    container,
   });
 
   generateFile('stories.js', {
     dir: componentDir,
+    outputFilename: `${slug}.stories.js`,
     slug,
     prefix,
+    container,
   });
+
+  if (container) {
+    generateFile('container.js', {
+      dir: componentDir,
+      outputFilename: `${slug}.container.js`,
+      slug,
+      prefix,
+      container,
+    });
+  }
 
   generateFile('style.styl', {
     dir: componentDir,
+    outputFilename: `${slug}.styl`,
     slug,
     prefix,
+    container,
   });
 };
 
